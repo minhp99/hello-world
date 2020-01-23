fe23b0d0169a24f288bc2721f203d713ac31154d
minhp99 <54794962+minhp99@users.noreply.github.com>
2020-01-23 12:27:57 +0100

    Add files via upload

---
+++ localhost-test4.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 25, 2019 at 12:20 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test1`
+--
+CREATE DATABASE IF NOT EXISTS `test1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `test1`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cmdcv`
+--
+
+CREATE TABLE `cmdcv` (
+  `mkdmc` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Database: `x`
+--
+CREATE DATABASE IF NOT EXISTS `x` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `x`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `c`
+--
+
+CREATE TABLE `c` (
+  `m` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `d`
+--
+
+CREATE TABLE `d` (
+  `v` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
